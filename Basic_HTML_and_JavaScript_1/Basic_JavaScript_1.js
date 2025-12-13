3099d04b3a506067f14c40af062df4a98064d2e1
Mike Busch <michael.john.busch@gmail.com>
2025-12-12 18:56:41 -0800

    Add Basic_HTML and JavaScript_1
    HTML & JavaScript Practice

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, Mike! I am an alert box!");
+
+// This function changes the text color to green
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = "<span style='color:green'>" + String + "</span>";
+		document.getElementById("Irish").innerHTML = result;
+}
